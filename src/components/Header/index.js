87eefbbfc879240b44670229fa85c24b3cc40fa4
CleitonBrito <cleytonbritto3003@gmail.com>
2024-09-13 21:09:06 -0300

    update text color header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,8 +12,8 @@ export function Header(){
         <header className="admin-header absolute top-10 right-2 md:right-20  flex sm:w-1/4 w-1/2 justify-end">
             <nav className="nav-header flex">
                 <div className='flex flex-col justify-center items-center gap-2'>
-                    <button className='' onClick={ handleLogout }>
-                        <p className='text-gray-900 font-bold text-white rounded-full p-3'>Sair</p>
+                    <button onClick={ handleLogout }>
+                        <p className='font-bold text-white rounded-full p-3'>Sair</p>
                     </button>
                 </div>
             </nav>
